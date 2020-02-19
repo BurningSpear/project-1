a36e84ee5696e23b5f7d084ca641da2a3faab21f
Frank K <kigenyifrank@johndeere.com>
2020-02-19 12:33:42 -0600

    commit-2 memory game layout

--- script/script.js
+++ script/script.js
@@ -1,1 +1,15 @@
+let clickedCard = null;
+let preventClick = false;
+let combosFound = 0;
+const colors = [
+'pink',
+'yellow',
+'red',
+'cyan',
+'blue',
+'purple',
+'orange',
+'green'
+];
+const cards = [...document.querySelectorAll('.backOfCard')];
 
